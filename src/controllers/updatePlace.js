f49a4ac58e8976424e7f90fc231b6f208bdcf05c
Donald Johnson <djjohnson380@gmail.com>
2026-04-17 10:17:30 -0700

    Add protected update place route

---
+++ src/controllers/updatePlace.js
@@ -0,0 +1,38 @@
+const Place = require("../models/place");
+
+const updatePlace = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+
+    const updates = {};
+
+    if (name !== undefined) {
+      updates.name = name.trim();
+    }
+
+    if (description !== undefined) {
+      updates.description = description.trim();
+    }
+
+    if (Object.keys(updates).length === 0) {
+      return res.status(400).json({ message: "No valid fields to update" });
+    }
+
+    const place = await Place.findOneAndUpdate(
+      { _id: id, user: req.user.id },
+      updates,
+      { new: true, runValidators: true },
+    );
+
+    if (!place) {
+      return res.status(404).json({ message: "Place not found" });
+    }
+
+    res.json(place);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { updatePlace };
--- src/routes/places.js
+++ src/routes/places.js
@@ -3,11 +3,13 @@ const protect = require("../middleware/auth");
 const { createPlace } = require("../controllers/createPlace");
 const { getPlaces } = require("../controllers/getPlaces");
 const { deletePlace } = require("../controllers/deletePlace");
+const { updatePlace } = require("../controllers/updatePlace");
 
 const router = express.Router();
 
 router.post("/", protect, createPlace);
 router.get("/", protect, getPlaces);
 router.delete("/:id", protect, deletePlace);
+router.patch("/:id", protect, updatePlace);
 
 module.exports = router;
