95f70a8286f51ce40616a1e43910e3d16aa91ba5
Donald Johnson <djjohnson380@gmail.com>
2026-04-27 11:45:55 -0700

    Add pagination to get places route

--- src/controllers/getPlaces.js
+++ src/controllers/getPlaces.js
@@ -2,7 +2,7 @@ const Place = require("../models/place");
 
 const getPlaces = async (req, res, next) => {
   try {
-    const { search, hasImage } = req.query;
+    const { search, hasImage, page, limit } = req.query;
 
     const query = { user: req.user.id };
 
@@ -21,11 +21,27 @@ const getPlaces = async (req, res, next) => {
       query.pictureUrl = "";
     }
 
-    const places = await Place.find(query).sort({
-      createdAt: -1,
-    });
+    const pageNumber = Math.max(Number(page) || 1, 1);
+    const limitNumber = Math.max(Number(limit) || 10, 1);
 
-    res.json(places);
+    const skip = (pageNumber - 1) * limitNumber;
+
+    const total = await Place.countDocuments(query);
+
+    const pages = Math.ceil(total / limitNumber);
+
+    const places = await Place.find(query)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limitNumber);
+
+    res.json({
+      page: pageNumber,
+      limit: limitNumber,
+      total,
+      pages,
+      places,
+    });
   } catch (err) {
     next(err);
   }
