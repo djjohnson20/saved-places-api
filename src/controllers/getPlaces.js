6245b3d12e6d0a682da44a69de298d9fda0d767b
Donald Johnson <djjohnson380@gmail.com>
2026-04-27 11:25:08 -0700

    Add search and image filters to get places route

--- src/controllers/getPlaces.js
+++ src/controllers/getPlaces.js
@@ -2,7 +2,26 @@ const Place = require("../models/place");
 
 const getPlaces = async (req, res, next) => {
   try {
-    const places = await Place.find({ user: req.user.id }).sort({
+    const { search, hasImage } = req.query;
+
+    const query = { user: req.user.id };
+
+    if (search && search.trim()) {
+      query.$or = [
+        { name: { $regex: search.trim(), $options: "i" } },
+        { description: { $regex: search.trim(), $options: "i" } },
+      ];
+    }
+
+    if (hasImage === "true") {
+      query.pictureUrl = { $ne: "" };
+    }
+
+    if (hasImage === "false") {
+      query.pictureUrl = "";
+    }
+
+    const places = await Place.find(query).sort({
       createdAt: -1,
     });
 
